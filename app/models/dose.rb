84be27b8c57f55919ffbf0ad304364d4b9d310a2
ttr42hozu <shigi10nato@gmail.com>
2021-01-26 19:02:31 +0900

    medicineとdoseのアソシエーション

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,8 +1,10 @@
 class Dose < ActiveHash::Base
   self.data = [
     {id: 1, name: '---'},
-    {id: 1, name: '1T'},
-    {id: 1, name: '1/2T'},
-    {id: 1, name: '1/4T'}
+    {id: 2, name: '1T'},
+    {id: 3, name: '1/2T'},
+    {id: 4, name: '1/4T'}
   ]
+  include ActiveHash::Associations
+  has_many :madecines
 end
--- app/models/medicine.rb
+++ app/models/medicine.rb
@@ -1,6 +1,7 @@
 class Medicine < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :usage
+  belongs_to :dose
 
   belongs_to :patient
   has_one :preparation
