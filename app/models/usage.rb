b8ecda8b50cff5a92d44c7e01c7998f33b888ef8
ttr42hozu <shigi10nato@gmail.com>
2021-01-26 16:15:51 +0900

    ActiveHash::Baseクラスの継承、usageデータの定義

--- app/models/usage.rb
+++ app/models/usage.rb
@@ -1,2 +1,9 @@
-class Usage < ApplicationRecord
+class Usage < ActiveHash::Base
+  self.data = [
+    { id = 1, name = '---'}
+    { id = 2, name = 'SID'}
+    { id = 3, name = 'BID'}
+    { id = 4, name = 'TID'}
+    { id = 5, name = 'EOD'}
+  ]
 end
