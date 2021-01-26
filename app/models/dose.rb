1a8af81f010f7cf116d4fb4c8234bcc789830688
ttr42hozu <shigi10nato@gmail.com>
2021-01-26 18:53:39 +0900

    ActiveHash::Baseクラスの継承、doseデータの定義

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,2 +1,8 @@
-class Dose < ApplicationRecord
+class Dose < ActiveHash::Base
+  self.data = [
+    {id: 1, name: '---'},
+    {id: 1, name: '1T'},
+    {id: 1, name: '1/2T'},
+    {id: 1, name: '1/4T'}
+  ]
 end
