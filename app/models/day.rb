1db0069942f276abdf55f748132fae1bce8fd394
ttr42hozu <shigi10nato@gmail.com>
2021-01-26 19:25:52 +0900

    ActiveHash::Baseクラスの継承、dayデータの定義

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,2 +1,9 @@
-class Day < ApplicationRecord
+class Day < ActiveHash::Base
+  self.data = [
+    {id: 1, name: '---'},
+    {id: 2, name: '7日'},
+    {id: 3, name: '14日'},
+    {id: 4, name: '28日'},
+    {id: 5, name: '56日'}
+  ]
 end
