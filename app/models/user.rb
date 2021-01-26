a71508e5df766548a6104355580f8bd27d4e2279
ttr42hozu <shigi10nato@gmail.com>
2021-01-26 15:42:45 +0900

    userモデル アソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :patients
+  has_many :preparations
   
   with_options presence: true do
     validates :nickname
