f997da3d942f5b1c7767e3fcd779c7c342ba04e3
ttr42hozu <shigi10nato@gmail.com>
2021-01-27 16:40:42 +0900

    各アクションとストロングストロングパラメータの定義

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -4,6 +4,23 @@ class PatientsController < ApplicationController
   end
 
   def new
-    
+    @patient_medicine = PatientMedicine.new
+
+  end
+
+  def create
+    @patient_medicine = PatientMedicine.new(patient_medicine_params)
+    if @patient_medicine.valid?
+      @patient_medicine.save
+      redirect_to root_path
+    else 
+      render action: :new
+    end
+  end
+
+  private
+  
+  def patient_medicine_params
+    params.require(:patient_medicine).permit(:image, :medical_record_number, :last_name, :first_name, :start_day, :name, :usage_id, :dose_id, :day_id).merge(user_id: current_user.id)
   end
 end
