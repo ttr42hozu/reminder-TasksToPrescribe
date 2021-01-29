6c0aa33949b34708c5e34172513c238b32b0485f
ttr42hozu <shigi10nato@gmail.com>
2021-01-29 19:16:08 +0900

    createアクション定義

--- app/controllers/preparations_controller.rb
+++ app/controllers/preparations_controller.rb
@@ -1,2 +1,17 @@
 class PreparationsController < ApplicationController
+  def create
+    @preparation = Preparation.new(preparation_params)
+    if @preparation.valid?
+      binding.pry
+      @preparation.save
+      redirect_to root_path
+    else
+      render root_path
+    end
+  end
+  private
+  
+  def preparation_params
+    params.permit(:medicine_id).merge(user_id: current_user.id)
+  end
 end
