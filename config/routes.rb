2faddd038d48e629ec70d6074cc21d30d44a0656
ttr42hozu <shigi10nato@gmail.com>
2021-02-01 18:30:23 +0900

    updateルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'patients#index'
   resources :patients, only: [:index, :new, :create] do
-    resources :preparations, only: [:create, :destroy]
+    resources :preparations, only: [:create, :destroy, :update]
   end
 end
