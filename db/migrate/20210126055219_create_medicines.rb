ce1e947359be06bafde930f81f76c4aa2aa399ff
ttr42hozu <shigi10nato@gmail.com>
2021-02-11 19:01:39 +0900

    カラム　修正（追加）

--- db/migrate/20210126055219_create_medicines.rb
+++ db/migrate/20210126055219_create_medicines.rb
@@ -5,6 +5,7 @@ class CreateMedicines < ActiveRecord::Migration[6.0]
       t.integer :usage_id,      null: false
       t.integer :dose_id,       null: false
       t.integer :day_id,       null: false
+      t.integer :start_day,       null: false
       t.references :patient, foreign_key: true
       t.timestamps
 
