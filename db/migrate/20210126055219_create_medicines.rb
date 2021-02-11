f3a0cb36086a5be8f97b71d139e1f6c3f03b6c4b
ttr42hozu <shigi10nato@gmail.com>
2021-02-11 19:24:58 +0900

    修正

--- db/migrate/20210126055219_create_medicines.rb
+++ db/migrate/20210126055219_create_medicines.rb
@@ -1,11 +1,11 @@
 class CreateMedicines < ActiveRecord::Migration[6.0]
   def change
     create_table :medicines do |t|
-      t.string :name,           null: false
-      t.integer :usage_id,      null: false
-      t.integer :dose_id,       null: false
-      t.integer :day_id,       null: false
-      t.integer :start_day,       null: false
+      t.string :name,        null: false
+      t.integer :usage_id,   null: false
+      t.integer :dose_id,    null: false
+      t.integer :day_id,     null: false
+      t.integer :start_day,  null: false
       t.references :patient, foreign_key: true
       t.timestamps
 
